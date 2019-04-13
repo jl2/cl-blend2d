28faf8dcf3d8ad381350bd9062022a87ca22dfd5
Jeremiah LaRocco <jeremiah_larocco@fastmail.com>
2019-04-13 12:53:01 -0600

    Successfully generated a wrapper using cl-autowrap, needs cleanup.
    Not sure how I feel about cl-autowrap's (autowrap:alloc) (autowrap:free), etc.
    To be a "good" Lisp library I'll have to create a higher level wrapper...

--- blend2d.lisp
+++ blend2d.lisp
@@ -17,32 +17,12 @@
 
 (in-package :blend2d)
 
-
-(defmacro define-cfun ((c-name name) return-type &body body)
-  `(progn
-     (defun ,name ,(loop for p in body
-                      with optional = nil
-                      with a = () with b = ()
-                      do (if (= (length p) 2) (progn
-                                                (when optional (error "why not optional?"))
-                                                (push (car p) a))
-                             (progn
-                               (setf optional t)
-                               (push (list (car p) (third p)) b)))
-                      finally (return (append (reverse a) (cons '&optional (reverse b)))))
-       (cffi:foreign-funcall
-        ,c-name
-        ,@(mapcan
-           #'(lambda (p)
-               (destructuring-bind (var type &rest r)
-                   p
-                 (declare (ignore r))
-                 (list type (case type
-                              (:float `(coerce ,var 'single-float))
-                              (:double `(coerce ,var 'double-float))
-                              (:int `(floor ,var))
-                              (otherwise var)))))
-           body)
-        ,return-type))
-     (export ',name)))
-
+(cffi:use-foreign-library "/home/jeremiah/oss_src/blend2d/blend2d/build/libblend2d.so")
+(autowrap:c-include "/home/jeremiah/oss_src/blend2d/blend2d/src/blend2d.h"
+                    :sysincludes #+linux(list "/usr/include/x86_64-linux-gnu/" "/usr/include/x86_64-linux-gnu/c++/7/")
+                    :language "c"
+                    :spec-path '(blend2d)
+                    :symbol-exceptions (("random" . "bl-random")
+                                        ("signal" . "bl-signal")
+                                        ("abort" . "bl-abort")
+                                        ("abs" . "bl-abs")))
